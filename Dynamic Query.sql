201b50f99d45c1f7b8059b000d015b580ec6cea4
ghbihuy <114790492+ghbihuy@users.noreply.github.com>
2022-10-01 10:40:09 +0700

    Add files via upload

---
+++ Dynamic Query.sql
@@ -0,0 +1,45 @@
+use lqn
+declare @col NVARCHAR(MAX);
+declare @col2 NVARCHAR(MAX) = '';
+declare @query NVARCHAR(MAX);
+declare @query2 NVARCHAR(MAX);
+
+--We have 3 way to make the quotename string agg of the column to pivot table
+--Way1: Using string_agg() function
+set @col =
+(select string_agg(quote,', ') within group (order by quote asc)
+from
+(select distinct quotename(order_priority) as quote
+from orders
+) t)
+
+--Way2: Using the loops of cursor
+select @col2 += QUOTENAME(order_priority) + ', ' 
+from 
+(select distinct order_priority
+from orders) pd
+set @col2 = LEFT(@col2, len(@col2) - 1)
+
+--Way3: using the loops of cusor and combine with coalesce() function
+declare @col3 NVARCHAR(MAX);
+select @col3 = coalesce(@col3 + ',','') +  QUOTENAME(order_priority)
+from (select distinct [order_priority] from orders) tab
+
+
+-- Dynamic Query
+SET @query2 = 
+'select province, '+@col+'
+from
+(select
+	od1.province,
+	od1.order_priority,
+	profit
+from orders od1
+) od2
+pivot
+(sum(profit)
+for order_priority in ('+@col+') 
+) as pivottable
+order by province asc'
+
+execute sp_executesql @query2
